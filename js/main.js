ad99c9cc6b32c39a8076973c7e4bd0eb901d4ecf
patryk797 <patrykskoczynski453@gmail.com>
2022-01-13 21:17:14 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+console.log('WARSZTAT - Zapis do newslettera!')
+
+let newsletterForm = document.getElementById('newsletter-form');
+let allAgreechx = document.getElementById('all-agree');
+
+const validate = (event) => {
+    
+    let txtName = document.getElementById('name');
+    let txtEmail = document.getElementById('email');
+    let agree1 = document.getElementById('agree-1');
+    let errors = document.getElementById('errors');
+
+    errors.innerHTML = '';
+
+    if (txtName.value.trim() === '') {
+        let liError = document.createElement('li');
+        liError.innerText = 'Wpisz Imie i nazwisko!';
+        errors.appendChild(liError);
+        
+    }
+
+    if (txtEmail.value.trim() === '') {
+        let liError = document.createElement('li');
+        liError.innerText = 'Wpisz adres e-mail!';
+        errors.appendChild(liError);
+
+    }
+
+    if (!txtEmail.value.includes('@')) {
+        let liError = document.createElement('li');
+        liError.innerText = 'Adres emaii musi zawierać @';
+        errors.appendChild(liError);
+    }
+
+    if (!agree1.checked) {
+        let liError = document.createElement('li');
+        liError.innerText = 'Nie wyraziłeś zgody 1 !';
+        errors.appendChild(liError);
+    }
+
+    if (errors.children.length > 0){
+        event.preventDefault();
+    }
+}
+
+
+const allAgree = (event) => {
+    let agree1 = document.getElementById('agree-1')
+    let agree2 = document.getElementById('agree-2')
+    
+    agree1.checked = event.target.checked
+    agree2.checked = event.target.checked
+
+    agree1.disabled = event.target.checked
+    agree2.disabled = event.target.checked
+    // console.log(event.target.checked);
+
+}
+
+newsletterForm.addEventListener('submit', validate);
+allAgreechx.addEventListener('change', allAgree);
